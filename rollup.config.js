2931b34a2688fbdeabbe2251edcb02c2b999706d
Eduards Egle <eduards@egle.me>
2020-10-20 21:50:50 +0300

    :sparkles: Add Tailwind

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import html from "@rollup/plugin-html";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
@@ -20,9 +23,25 @@ export default [
     },
     plugins: [
       svelte({
-        preprocess: sveltePreprocess(),
+        preprocess: sveltePreprocess({
+          postcss: postcss({
+            plugins: [tailwindcss, autoprefixer],
+          }),
+        }),
+
+        // Extract CSS into a single bundled file (recommended).
+        // See note below
+        css: function (css) {
+          // creates `main.css` and `main.css.map`
+          // using a falsy name will default to the bundle name
+          // — pass `false` as the second argument if you don't want the sourcemap
+          css.write("main.css");
+        },
       }),
-      resolve(),
+      resolve({
+        browser: true,
+        dedupe: ["svelte"],
+      }),
 
       // A Rollup plugin to convert CommonJS modules to ES6, so they can be
       // included in a Rollup bundle
--- src/Blog.svelte
+++ src/Blog.svelte
@@ -2,6 +2,12 @@
   import { Link } from "svelte-routing";
 </script>
 
+<style>
+  h1 {
+    color: green;
+  }
+</style>
+
 <h1>Blog test</h1>
 
 <Link to="blog/1">BlogPost</Link>
